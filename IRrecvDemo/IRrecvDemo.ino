11fd0f117f98aa62e6dd33245a66649d54fcf353
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2025-04-22 08:45:58 +0700

    updates

---
+++ IRrecvDemo/IRrecvDemo.ino
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+
+const uint16_t kRecvPin = 14;
+
+IRrecv irrecv(kRecvPin);
+decode_results results;
+
+void setup() {
+  Serial.begin(115200);
+  irrecv.enableIRIn();
+  while (!Serial)
+    delay(50);
+  Serial.println();
+  Serial.print("IRrecvDemo is now running and waiting for IR message on Pin ");
+  Serial.println(kRecvPin);
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    serialPrintUint64(results.value, HEX);
+    Serial.println("");
+    irrecv.resume();
+  }
+  delay(100);
+}
